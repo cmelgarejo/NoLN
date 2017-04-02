e9a92e320ccaef083c234410bac02bf10eef0d08
Jony Funes <hookode@outlook.com>
2017-04-02 01:52:03 -0400

    First entry

---
+++ filtro.js
@@ -0,0 +1,28 @@
+( function() {
+    var observer = new MutationObserver( function(mutations) {
+        mutations.forEach(function(mutation) {
+            var newNodes = mutation.addedNodes;
+            
+            if (newNodes !== null) {
+                var nodes = document.querySelectorAll('.fbUserContent, .userContentWrapper, ._1bar, ._5my2, ._4qjp, ._2kg4, ._4-u2');
+                
+                for (var ii = 0, nn = nodes.length; ii < nn; ii++) {
+                    var text = nodes[ii] ? nodes[ii].textContent.toLowerCase() : '';
+
+                    if (text && text.indexOf('lanacion') >= 0 && nodes[ii].style.display != 'none') {
+                        nodes[ii].style.display = 'none';
+                        chrome.runtime.sendMessage({action: "removeTrump"});
+                    }
+                }
+            }
+        });
+    });
+
+    observer.observe(document, {
+        childList: true,
+        subtree: true,
+        attributes: false,
+        characterData: false,
+    });
+
+})();
